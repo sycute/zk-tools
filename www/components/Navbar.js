7ed96b51f2733b5d28c1423a51e4180aa161a1e2
zhouchongshen <zhouchongshen@qq.com>
2025-01-13 15:45:32 +0800

    导航栏

---
+++ www/components/Navbar.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+const nav = [
+  {
+    title: "SEND",
+    href: "/send",
+    description: "",
+  },
+  {
+    title: "APPS",
+    href: "/receive",
+    description: "",
+  },
+  {
+    title: "HISTORY",
+    href: "/",
+    description: "",
+  },
+];
+const Navbar = () => {
+  return (
+    <header className="flex justify-between items-center mx-auto px-4 lg:px-0 max-w-[1440px]">
+      <h1 className="text-2xl">Next.js</h1>
+      <nav>
+        <ul className="flex gap-4">
+          {nav.map((item) => (
+            <li key={item.title}>
+              <Link href={item.href}>{item.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <ConnectButton connectText="连接钱包" style={{ color: "red" }} />
+    </header>
+  );
+};
