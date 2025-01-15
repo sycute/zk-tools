af925f7d8b73f9c3f938e54876429d5da15c57b2
zhouchongshen <zhouchongshen@qq.com>
2025-01-15 08:39:43 +0800

    使用Form表单验证

--- www/components/drawBody.js
+++ www/components/drawBody.js
@@ -1,3 +1,4 @@
+"use client";
 import {
   useSignTransactionBlock,
   useSuiClient,
@@ -7,13 +8,14 @@ import {
 } from "@mysten/dapp-kit";
 import { useEffect, useState } from "react";
 import { useCoinBalances } from "@/lib/useUserBalance";
+import { Button, Checkbox, Form, Input } from "antd";
 const DrawBody = () => {
   const client = useSuiClient();
   const [coinTypes, setCoinTypes] = useState([]);
   const [coinInfo, setCoinInfo] = useState({});
   const [editStates, setEditStates] = useState([]); // 存储每个元素是否处于编辑状态
   const currentAccount = useCurrentAccount();
-
+  const [form] = Form.useForm();
   useEffect(() => {
     const getTypes = async () => {
       // 获取所有CoinType
@@ -48,7 +50,6 @@ const DrawBody = () => {
       if (!coinTypes.length) return;
       coinTypes.forEach(async (item) => {
         const res = await getCoinInfo(item.fullType);
-        console.log(res);
 
         setCoinInfo((prevDecimals) => {
           let data = { ...prevDecimals };
@@ -58,55 +59,90 @@ const DrawBody = () => {
       });
     };
     getAllCoinInfo();
-    setEditStates(coinTypes.map(() => false))
+    setEditStates(coinTypes.map(() => false));
   }, [coinTypes]);
 
-  useEffect(() => {
-    console.log(coinInfo);
-    console.log(coinTypes);
-  }, [coinInfo]);
+  //   useEffect(() => {
+  //     console.log(coinInfo);
+  //     console.log(coinTypes);
+  //   }, [coinInfo]);
 
-    // 切换到编辑模式
-    
+  // 切换到编辑模式
+  const handleEdit = (index) => {
+    setEditStates((prev) =>
+      prev.map((editState, i) => (i === index ? true : editState))
+    );
+  };
+  function checkNaN(value) {
+    return Number.isNaN(value) ? 0 : value;
+  }
+  // 手动触发验证
+  const handleBlur = () => {
+    form.validateFields();
+  };
   return (
     <div className="w-full h-full flex flex-col justify-start items-center">
       {/* 标题 */}
       <div className="text-4xl font-extrabold mb-4">Choose coins</div>
       {/* 选择栏 */}
       <div className="w-full   bg-slate-100 rounded-3xl p-4">
-        {coinTypes.map((item, index) => {
-          return (
-            <div
-              className="w-full h-16  flex justify-start items-center border-b-[1px] last:border-0 mt-4 pb-4 first:mt-0 last:pb-0"
-              key={item.fullType}
-            >
-              {/* icon图 */}
-             
-              <div
-                style={{
-                  backgroundImage: `url(${coinInfo[item.type]?.iconUrl})`,
-                }}
-                className={`bg-no-repeat  bg-center bg-cover h-14 w-14 mr-3`}
-              ></div>
-              <div className="h-14 flex-grow flex flex-col justify-around">
-                {/* 标题 */}
-                {editStates[index] ? (
-                  <input
-                    type="text"
-                    placeholder={item.type}
-                    autoFocus
-                    className="border border-gray-300 rounded px-2 py-1"
-                  />
-                ) : (
-                  <div className="text-xl font-bold cursor-pointer"  onClick={() => handleEdit(index)} >{item.type}</div>
-                )}
+        <Form>
+          {coinTypes.map((item, index) => {
+            console.log(
+              typeof (item?.balance - 0) /
+                10 ** coinInfo[item.type]?.decimals ==
+                "NaN"
+            );
 
-                {/* 余额 */}
-                <div className="text-sm text-gray-500">1234.56789 HUSKI</div>
+            return (
+              <div
+                className="w-full  flex justify-start items-center border-b-[1px] last:border-0 mt-4 pb-6 first:mt-0 last:pb-0"
+                key={item.fullType}
+              >
+                {/* icon图 */}
+
+                <div
+                  style={{
+                    backgroundImage: `url(${coinInfo[item.type]?.iconUrl})`,
+                  }}
+                  className={`bg-no-repeat  bg-center bg-cover h-14 w-14 mr-3`}
+                ></div>
+
+                <div className="h-14 flex-grow flex flex-col justify-start">
+                  {/* 标题 */}
+                  {editStates[index] ? (
+                    <Form.Item
+                      name={item.type}
+                      rules={[
+                        {
+                          required: true,
+                          message: "Please input your username!",
+                        },
+                      ]}
+                      className="mb-3"
+                    >
+                      <Input onBlur={handleBlur} />
+                    </Form.Item>
+                  ) : (
+                    <div
+                      className="text-xl font-bold cursor-pointer"
+                      onClick={() => handleEdit(index)}
+                    >
+                      {item.type}
+                    </div>
+                  )}
+
+                  {/* 余额 */}
+                  <div className="text-sm text-gray-500">
+                    {checkNaN(
+                      (item.balance - 0) / 10 ** coinInfo[item.type]?.decimals
+                    )}
+                  </div>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </Form>
       </div>
     </div>
   );
