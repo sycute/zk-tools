0e17409becba1a067fd595705520e07b00471be7
ly <usocute@foxmail.com>
2025-01-20 10:48:39 +0800

    add cors

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,9 +1,17 @@
 use psw_gift::apis::*;
-use salvo::prelude::*;
+use salvo::{
+    cors::{AllowHeaders, AllowMethods, AllowOrigin, Cors},
+    prelude::*,
+};
 
 #[shuttle_runtime::main]
 async fn salvo() -> shuttle_salvo::ShuttleSalvo {
+    let cors = Cors::new()
+        .allow_origin(AllowOrigin::any())
+        .allow_methods(AllowMethods::any())
+        .allow_headers(AllowHeaders::any());
     let router = Router::new()
+        .hoop(cors.into_handler())
         .push(Router::with_path("zkrpnew").get(create_zkrp))
         .push(Router::with_path("zkrpclaim").get(claim_zkrp));
 
